d4bc48c90565b020ff9f13f46ba7df8fc326bcab
Filip Danielsson <filip.danielsson@tui.se>
2018-10-19 14:31:04 +0200

    yup

--- routes/staff.js
+++ routes/staff.js
@@ -1,5 +1,7 @@
 const router = require('koa-better-router')().loadMethods()
 const mongo = require('../mongo')
+const staffValidation = require('../validations/staffValidation')
+const logger = require('tuin-logging')
 
 router.get('/staff/:id', async (ctx, next) => {
     ctx.body = {
@@ -12,17 +14,36 @@ router.get('/staff/:id', async (ctx, next) => {
 router.post('/staff', async (ctx, next) => {
     const body = ctx.request.body
 
-    const entity = {
+    const model = {
         name: body.name
     }
 
-    console.log(entity)
+    const validation = await staffValidation.validate(model, { abortEarly: false }).catch(function(err) {
+        return err
+    })
+
+    if (validation.errors !== undefined && validation.errors.length > 0) {
+        logger.warning('Staff schema validation failed, aborting', model)
+
+        ctx.body = {
+            ok: false,
+            errors: validation.errors
+        }
+
+        return await next()
+    }
+
+    // await mongo
+    //     .collection('tips')
+    //     .find({ brandid: ctx.params.brandid })
+    //     .project({ id: 1, numberOfItems: 1 })
+    //     .toArray()
 
     ctx.body = {
         ok: true
     }
 
-    await next()
+    return await next()
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require('./app')
+const logger = require('tuin-logging')
 
 const port = process.env.SERVICE_PORT || 5000
 
 app.listen(port, () => {
-    console.log('Express server listening on port ' + port)
+    logger.info('Express server listening on port ' + port)
 })
