922130eb6d70c0296bd3778e0c0d3ae18745e30b
Reft <filipdanielsson@hotmail.se>
2018-10-24 14:04:30 +0200

    modified validation

--- validations/staffValidation.js
+++ validations/staffValidation.js
@@ -5,7 +5,20 @@ const staffValidation = Yup.object().shape({
     // gender,
     id: Yup.string()
         .nullable(true)
-        .required('Id is required')
+        .required('Id is required'),
+    name: Yup.string()
+        .nullable(true)
+        .required('Name is required'),
+    dateOfBirth: Yup.date()
+        .typeError('Date of birth must be a datetime')
+        .nullable(true)
+        .required('Date of birth is required'),
+    sourceMarket: Yup.string()
+        .nullable(true)
+        .required('Source market is required'),
+    phone: Yup.string()
+        .nullable(true)
+        .required('Phone is required')
 })
 
 module.exports = staffValidation
