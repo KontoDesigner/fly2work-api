039c19e7581dbf3d4c35e11574f90cf0c256ba5e
Filip Danielsson <filip.danielsson@tui.se>
2018-10-29 16:49:00 +0100

    constants undef

--- infrastructure/constants.js
+++ infrastructure/constants.js
@@ -1,25 +1,25 @@
 const keyMirror = require('keymirror')
 
 function Staff() {
-    this.id = null
-    this.name = null
-    this.dateOfBirth = null
-    this.sourceMarket = null
-    this.hotelStart = null
-    this.hotelEnd = null
-    this.dateOfFlight = null
-    this.role = null
-    this.departureAirport = null
-    this.arrivalAirport = null
-    this.typeOfFlight = null
-    this.comment = null
-    this.hotelNeeded = null
-    this.phone = null
-    this.status = null
-    this.gender = null
-    this.destination = null
-    this.positionStart = null
-    this.statusUpdated = null
+    this.id = undefined
+    this.name = undefined
+    this.dateOfBirth = undefined
+    this.sourceMarket = undefined
+    this.hotelStart = undefined
+    this.hotelEnd = undefined
+    this.dateOfFlight = undefined
+    this.role = undefined
+    this.departureAirport = undefined
+    this.arrivalAirport = undefined
+    this.typeOfFlight = undefined
+    this.comment = undefined
+    this.hotelNeeded = undefined
+    this.phone = undefined
+    this.status = undefined
+    this.gender = undefined
+    this.destination = undefined
+    this.positionStart = undefined
+    this.statusUpdated = undefined
 }
 
 function Email() {
