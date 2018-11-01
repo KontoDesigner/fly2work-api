dd9b1db05e1eff971948dc42e55d7f29d47dcf77
Filip Danielsson <filip.danielsson@tui.se>
2018-11-01 14:31:48 +0100

    modified new validation

--- validations/newValidation.js
+++ validations/newValidation.js
@@ -1,27 +1,9 @@
 const Yup = require('yup')
 
 const newValidation = Yup.object().shape({
-    // hotelNeeded,
-    // gender,
     id: Yup.string()
         .nullable(true)
         .required('Id is required'),
-    name: Yup.string()
-        .nullable(true)
-        .required('Name is required'),
-    dateOfBirth: Yup.date()
-        .typeError('Date of birth must be a datetime')
-        .nullable(true)
-        .required('Date of birth is required'),
-    sourceMarket: Yup.string()
-        .nullable(true)
-        .required('Source market is required'),
-    phone: Yup.string()
-        .nullable(true)
-        .required('Phone is required'),
-    gender: Yup.string()
-        .nullable(true)
-        .required('Gender is required'),
     destination: Yup.string()
         .nullable(true)
         .required('Destination is required'),
