2595d72e72119e1421f6385a757bf9621704a238
Reft <filipdanielsson@hotmail.se>
2018-11-05 17:12:10 +0100

    attachment stuff

--- routes/attachment.js
+++ routes/attachment.js
@@ -2,7 +2,8 @@ const router = require('koa-better-router')().loadMethods()
 const logger = require('tuin-logging')
 const body = require('koa-better-body')
 const mongo = require('../infrastructure/mongo')
-const Binary = require('mongodb').Binary
+const moment = require('moment')
+const uuid = require('node-uuid')
 const fs = require('fs')
 
 const BASE = '/attachment'
@@ -10,26 +11,40 @@ const BASE = '/attachment'
 router.post(`${BASE}/upload`, body(), function*(next) {
     const staffId = this.request.fields.staffId
     const file = this.request.fields.file[0]
-    const params = { id: staffId, name: file.name, size: file.size, type: file.type, path: file.path }
 
-    logger.info('Uploading attachment..', params)
+    logger.info('Uploading attachment..', { id: staffId, name: file.name, size: file.size, type: file.type, path: file.path })
 
     let res = false
+
     try {
-        var data = fs.readFileSync(file.path)
+        const attachment = {
+            id: uuid.v1(),
+            data: fs.readFileSync(file.path),
+            name: file.name,
+            size: file.size,
+            type: file.type,
+            created: moment()._d
+        }
 
         mongo
             .collection('staffs')
-            .updateOne({ id: staffId }, { $push: { attachments: Binary(data) } })
+            .updateOne({ id: staffId }, { $push: { attachments: attachment } })
             .then(function(result) {
                 if (result.ok === 1) {
-                    logger.info('Upload attachment successfull', params, result)
+                    logger.info('Upload attachment successfull', {
+                        id: staffId,
+                        name: file.name,
+                        size: file.size,
+                        type: file.type,
+                        path: file.path,
+                        result
+                    })
 
                     res = true
                 }
             })
     } catch (err) {
-        logger.error('Error uploading attachment', err, params)
+        logger.error('Error uploading attachment', err, { id: staffId, name: file.name, size: file.size, type: file.type, path: file.path })
     }
 
     this.body = {
@@ -40,8 +55,54 @@ router.post(`${BASE}/upload`, body(), function*(next) {
 })
 
 router.post(`${BASE}/download`, async (ctx, next) => {
+    const staffId = ctx.request.body.staffId
+    const id = ctx.request.body.id
+
+    logger.info('Downloading attachment..', { staffId, id })
+
+    const attachment = await mongo.collection('staffs').findOne(
+        {
+            id: staffId,
+            'attachments.id': id
+        },
+        {
+            'attachments.$': 1
+        }
+    )
+
+    if (attachment) {
+        logger.info('Found attachment for download', { staffId, id, attachment })
+    } else {
+        logger.info('Could not find attachment for download', { staffId, id })
+    }
+
+    ctx.body = attachment
+
+    await next()
+})
+
+router.post(`${BASE}/delete`, async (ctx, next) => {
+    const staffId = ctx.request.body.staffId
+    const id = ctx.request.body.id
+
+    logger.info('Deleting attachment..', { staffId, id })
+
+    let res = false
+
+    try {
+        const result = await mongo.collection('staffs').updateOne({ id: staffId }, { $pull: { id: id } })
+
+        if (result.ok === 1) {
+            logger.info('Deleted attachment', { staffId, id, result })
+
+            res = true
+        }
+    } catch (err) {
+        logger.error('Error deleting attachment', err, { id })
+    }
+
     ctx.body = {
-        ok: true
+        ok: res
     }
 
     await next()
